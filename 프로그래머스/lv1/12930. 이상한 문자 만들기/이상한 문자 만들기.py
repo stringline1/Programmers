d93ee8284bc533687c10b99b65dfee18d89c703a
stringline1 <96370932+stringline1@users.noreply.github.com>
2022-12-30 17:28:45 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    s = s.split(' ')
+    answer = ''
+    for word in s:
+        for i in range(len(word)):
+            if i%2 == 0:
+                answer += word[i].upper()
+            else:
+                answer += word[i].lower()
+        answer += ' '
+    return answer[0:-1]
