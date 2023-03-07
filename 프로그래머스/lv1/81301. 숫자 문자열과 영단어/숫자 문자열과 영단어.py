1b573cb1ca88c9ea0b2964c2040dafb49e3af84a
stringline1 <96370932+stringline1@users.noreply.github.com>
2023-03-07 16:28:37 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,7 @@
+def solution(s):
+    number = {"zero":0, "one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9}
+    
+    for i in number.keys():
+        s = s.replace(i, str(number[i]))
+        
+    return int(s)
